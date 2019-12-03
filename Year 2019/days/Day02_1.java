30cfd5054729a4e7339493cf383e21f52e3bedfb
Alan <alanxiao2013@gmail.com>
2019-12-03 19:36:24 +0800

    Refactor (Move) code into Year 2019 folder

--- Year 2019/days/Day02_1.java
+++ Year 2019/days/Day02_1.java
@@ -22,13 +22,18 @@ public class Day02_1 {
 			temp.add(Integer.parseInt(scanner.next(",")));
 		}
 		temp.add(Integer.parseInt(scanner.next()));
+		scanner.close();
 		// transfer ArrayList into int Array
 		int[] array = new int[temp.size()];
 		for(int x = 0; x < array.length; x++) {
 			array[x] = temp.get(x);
 		}
-		
-		
+		// start code
+		for(int position = 0; position < array.length; position += 4) {
+			
+		}
+		// print final answer, number at array[0]
+		System.out.println(array[0]);
 	}
 	
 }
